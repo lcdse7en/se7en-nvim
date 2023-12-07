dcf64798a34976a0cf9f4887d6337c20bef9cf9d
lcdse7en <2353442022@qq.com>
2023-12-08 04:11:28 +0800

    add

--- lua/config/plugins/indent-blankline.lua
+++ lua/config/plugins/indent-blankline.lua
@@ -1,31 +1,49 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
   enabled = true,
+  main = 'ibl',
   event = { 'BufReadPost', 'BufNewFile' },
   config = function()
+    local exclude_ft = {
+      "help",
+      "git",
+      "markdown",
+      "snippets",
+      "text",
+      "gitconfig",
+      "alpha",
+      "dashboard",
+      "sh",
+    }
     require('ibl').setup({
-      indent = { char = '▏' },
+      indent = {
+        char = "│",
+      },
       scope = {
         show_start = false,
         show_end = false,
       },
       exclude = {
-        filetypes = {
-          'help',
-          'alpha',
-          'dashboard',
-          '*oil*',
-          'neo-tree',
-          'Trouble',
-          'lazy',
-          'mason',
-          'notify',
-          'toggleterm',
-          'lazyterm',
-          'asm',
-        },
+        filetypes = exclude_ft,
+        buftypes = { "terminal" },
       },
     })
+
+    local gid = vim.api.nvim_create_augroup("indent_blankline", { clear = true })
+    vim.api.nvim_create_autocmd("InsertEnter", {
+      pattern = "*",
+      group = gid,
+      command = "IBLDisable",
+    })
+
+    vim.api.nvim_create_autocmd("InsertLeave", {
+      pattern = "*",
+      group = gid,
+      callback = function()
+        if not vim.tbl_contains(exclude_ft, vim.bo.filetype) then
+          vim.cmd [[IBLEnable]]
+        end
+      end,
+    })
   end,
-  main = 'ibl',
 }
