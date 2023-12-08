0f747bf2439d699db30b5be7267354de287654fe
lcdse7en <2353442022@qq.com>
2023-12-08 18:24:41 +0800

    add

--- lua/config/plugins/comment.lua
+++ lua/config/plugins/comment.lua
@@ -2,6 +2,20 @@ return {
   'numToStr/Comment.nvim',
   enabled = true,
   event = { 'BufReadPre' },
+  keys = {
+    {
+      '<leader>cc',
+      '<cmd>lua require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)<cr>',
+      mode = { 'n' },
+      desc = 'Toggle comment line',
+    },
+    {
+      '<leader>cc',
+      '<esc><cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<cr>',
+      mode = { 'v' },
+      desc = 'Toggle comment for selection',
+    },
+  },
   dependencies = {
     {
       'JoosepAlviste/nvim-ts-context-commentstring',
@@ -11,8 +25,8 @@ return {
   config = function()
     -- to skip backwards compatibility routines and speed up loading
     vim.g.skip_ts_context_commentstring_module = true
-    require('Comment').setup({
+    require('Comment').setup {
       pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-    })
+    }
   end,
 }
