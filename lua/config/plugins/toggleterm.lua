059d6cf6c8abd8c1704d79af792287e61eba508d
lcdse7en <2353442022@qq.com>
2023-12-08 20:47:12 +0800

    add

--- lua/config/plugins/toggleterm.lua
+++ lua/config/plugins/toggleterm.lua
@@ -1,11 +1,11 @@
 return {
   'akinsho/toggleterm.nvim',
   enabled = true,
-  version = "*",
+  version = '*',
   cmd = 'ToggleTerm',
   keys = { '<leader>ot' },
   config = function()
-    require("toggleterm").setup {
+    require('toggleterm').setup {
       size = 20,
       open_mapping = [[<f12>]],
       hide_numbers = true,
@@ -18,24 +18,24 @@ return {
       close_on_exit = true,
       shell = vim.o.shell,
       -- highlights = {}, -- Not working
-    --   float_opts = {
-    --     border = { '', '', '', ' ', ' ', ' ', ' ', ' ' },
-    --     relative = 'editor',
-    --     width = vim.opt.columns:get(),
-    --     height = 20,
-    --     col = 0,
-    --     row = vim.opt.lines:get(),
-    --     style = 'minimal',
-    --     title = 'Hello KITTY',
-    --     title_pos = 'left',
-    --     noautocmd = true,
-    --   },
-    --   on_open = function(term)
-    --     local ns = vim.api.nvim_create_namespace('ToggleTerm')
-    --     vim.api.nvim_set_hl(ns, 'Normal', { link = 'TTNormal' })
-    --     vim.api.nvim_set_hl(ns, 'FloatBorder', { link = 'TTBorder' })
-    --     vim.api.nvim_win_set_hl_ns(term.window, ns)
-    --   end,
+      --   float_opts = {
+      --     border = { '', '', '', ' ', ' ', ' ', ' ', ' ' },
+      --     relative = 'editor',
+      --     width = vim.opt.columns:get(),
+      --     height = 20,
+      --     col = 0,
+      --     row = vim.opt.lines:get(),
+      --     style = 'minimal',
+      --     title = 'Hello KITTY',
+      --     title_pos = 'left',
+      --     noautocmd = true,
+      --   },
+      --   on_open = function(term)
+      --     local ns = vim.api.nvim_create_namespace('ToggleTerm')
+      --     vim.api.nvim_set_hl(ns, 'Normal', { link = 'TTNormal' })
+      --     vim.api.nvim_set_hl(ns, 'FloatBorder', { link = 'TTBorder' })
+      --     vim.api.nvim_win_set_hl_ns(term.window, ns)
+      --   end,
     }
   end,
 }
