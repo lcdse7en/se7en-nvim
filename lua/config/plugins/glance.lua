54c0d20d432890511b8afc21df9954524cae3822
lcdse7en <2353442022@qq.com>
2023-12-26 00:20:27 +0800

    add

--- lua/config/plugins/glance.lua
+++ lua/config/plugins/glance.lua
@@ -5,27 +5,25 @@
 -- Description :                             *
 --********************************************
 
-
-
 return {
   'dnlhc/glance.nvim',
-  cmd = { "Glance" },
+  cmd = { 'Glance' },
   keys = {
-    { "gd", "<cmd>Glance definitions<CR>", desc = "LSP Definition" },
-    { "gr", "<cmd>Glance references<CR>", desc = "LSP References" },
-    { "gi", "<cmd>Glance implementations<CR>", desc = "LSP Implementations" },
-    { "gt", "<cmd>Glance type_definitions<CR>", desc = "LSP Type Definitions" },
+    { 'gd', '<cmd>Glance definitions<CR>', desc = 'LSP Definition' },
+    { 'gr', '<cmd>Glance references<CR>', desc = 'LSP References' },
+    { 'gi', '<cmd>Glance implementations<CR>', desc = 'LSP Implementations' },
+    { 'gt', '<cmd>Glance type_definitions<CR>', desc = 'LSP Type Definitions' },
   },
   config = function()
-    local filter = require("lsp.utils.filter").filter
-    local filterReactDTS = require("lsp.utils.filterReactDTS").filterReactDTS
+    local filter = require('lsp.utils.filter').filter
+    local filterReactDTS = require('lsp.utils.filterReactDTS').filterReactDTS
 
-    require("glance").setup {
+    require('glance').setup {
       hooks = {
         before_open = function(results, open, jump, method)
           if #results == 1 then
             jump(results[1]) -- argument is optional
-          elseif method == "definitions" then
+          elseif method == 'definitions' then
             results = filter(results, filterReactDTS)
             open(results)
           else
