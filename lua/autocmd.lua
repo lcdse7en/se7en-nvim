198b2975db9137a05b6b51fdba3233f6d8ad63c0
lcdse7en <2353442022@qq.com>
2024-01-07 08:52:57 +0800

    add

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -706,14 +706,14 @@ vim.api.nvim_create_autocmd('UILeave', {
 })
 
 vim.cmd [[
+  augroup _fold_bug_solution "https://github.com/nvim-telescope/telescope.nvim/issues/559"
+    autocmd!
+    autocmd BufRead * autocmd BufWinEnter * ++once normal! zx
+  augroup end
+
   augroup remember_folds
     autocmd!
     autocmd BufWinLeave * mkview
     autocmd BufEnter * silent! loadview
-  augroup end
-
-  augroup _fold_bug_solution "https://github.com/nvim-telescope/telescope.nvim/issues/559"
-    autocmd!
-    autocmd BufRead * autocmd BufWinEnter * ++once normal! zx
   augroup end
 ]]
