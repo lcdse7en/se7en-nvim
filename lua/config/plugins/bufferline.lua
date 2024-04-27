504da46b3e9e6600f0673df565dded1a95cb26ff
lcdse7en <2353442022@qq.com>
2024-04-27 20:29:42 +0800

    add

--- lua/config/plugins/bufferline.lua
+++ lua/config/plugins/bufferline.lua
@@ -1,83 +1,47 @@
 return {
   'akinsho/bufferline.nvim',
-  event = { 'BufReadPost', 'BufAdd', 'BufNewFile' },
-  enabled = false,
+  event = 'VeryLazy',
+  enabled = true,
   dependencies = {
     'nvim-tree/nvim-web-devicons',
     'echasnovski/mini.bufremove',
   },
   version = '*',
   config = function()
-    local icons = { ui = require('modules.utils.icons').get 'ui' }
+    local bufferline = require 'bufferline'
 
-    local opts = {
+    bufferline.setup {
       options = {
-        number = nil,
-        close_command = 'BufDel! %d',
-        right_mouse_command = 'BufDel! %d',
-        modified_icon = icons.ui.Modified,
-        buffer_close_icon = icons.ui.Close,
-        left_trunc_marker = icons.ui.Left,
-        right_trunc_marker = icons.ui.Right,
-        max_name_length = 20,
-        max_prefix_length = 13,
-        tab_size = 20,
-        color_icons = true,
-        show_buffer_icons = true,
-        show_buffer_close_icons = true,
-        show_close_icon = true,
-        show_tab_indicators = true,
-        enforce_regular_tabs = false,
-        persist_buffer_sort = true,
+        close_command = function(n)
+          require('mini.bufremove').delete(n, false)
+        end,
+        right_mouse_command = function(n)
+          require('mini.bufremove').delete(n, false)
+        end,
+        show_buffer_close_icons = false,
+        -- separator_style = { '|', '|' },
+        separator_style = { '', '' },
         always_show_bufferline = true,
-        separator_style = 'thin',
-        diagnostics = 'nvim_lsp',
-        diagnostics_indicator = function(count)
-          return '(' .. count .. ')'
+        style_preset = bufferline.style_preset.no_italic,
+        numbers = function(opts)
+          return string.format('%s', opts.ordinal)
+        end,
+        custom_filter = function(buf_number)
+          -- filter out filetypes you don't want to see
+          if vim.bo[buf_number].filetype ~= 'qf' then
+            return true
+          end
         end,
         offsets = {
           {
-            filetype = 'NvimTree',
+            filetype = 'NeoTree',
             text = 'File Explorer',
+            highlight = 'Directory',
             text_align = 'center',
-            padding = 0,
-          },
-          {
-            filetype = 'aerial',
-            text = 'Symbol Outline',
-            text_align = 'center',
-            padding = 0,
+            separator = true,
           },
         },
       },
-      -- Change bufferline's highlights here! See `:h bufferline-highlights` for detailed explanation.
-      -- Note: If you use catppuccin then modify the colors below!
-      highlights = {},
     }
-
-    if vim.g.colors_name:find 'catppuccin' then
-      local cp = require('modules.utils').get_palette() -- Get the palette.
-
-      local catppuccin_hl_overwrite = {
-        highlights = require('catppuccin.groups.integrations.bufferline').get {
-          styles = { 'italic', 'bold' },
-          custom = {
-            all = {
-              -- Hint
-              hint = { fg = cp.rosewater },
-              hint_visible = { fg = cp.rosewater },
-              hint_selected = { fg = cp.rosewater },
-              hint_diagnostic = { fg = cp.rosewater },
-              hint_diagnostic_visible = { fg = cp.rosewater },
-              hint_diagnostic_selected = { fg = cp.rosewater },
-            },
-          },
-        },
-      }
-
-      opts = vim.tbl_deep_extend('force', opts, catppuccin_hl_overwrite)
-    end
-
-    require('modules.utils').load_plugin('bufferline', opts)
   end,
 }
