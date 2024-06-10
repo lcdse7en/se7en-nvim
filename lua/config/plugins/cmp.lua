8a9c0bc066270100a5b475cc5c6f35d893d4bea3
lcdse7en <2353442022@qq.com>
2024-06-11 07:16:33 +0800

    add

--- lua/config/plugins/cmp.lua
+++ lua/config/plugins/cmp.lua
@@ -103,6 +103,7 @@ return {
         '~/.config/nvim/lua/snippets/',
       },
     }
+
     -- require('luasnip.loaders.from_vscode').lazy_load()
     -- require('luasnip.loaders.from_snipmate').lazy_load {
     --   paths = { './snips/' },
