6dd436ee128fc3745750c042b77aded151e56351
lcdse7en <2353442022@qq.com>
2024-06-21 17:04:44 +0800

    add

--- lua/config/plugins/db.lua
+++ lua/config/plugins/db.lua
@@ -20,7 +20,7 @@ return {
     vim.g.db_ui_use_postgres_views = 1
     vim.g.db_ui_force_echo_notifications = 1
     vim.g.db_ui_show_help = 0
-    vim.g.db_ui_win_position = 'right'
+    vim.g.db_ui_win_position = 'left'
     vim.g.db_ui_use_nerd_fonts = 1
     vim.g.db_ui_use_nvim_notify = 1
     vim.g.db_ui_execute_on_save = false
